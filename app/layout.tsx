4619087a65664659fc6fdbe51ce3e7e2d3e059d2
Laurence Liang <laurence.w.liang@gmail.com>
2026-08-04 01:07:29 +0200

    chore: update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     default: 'Laurence Liang',
     template: '%s'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description: "Laurence's Website",
 };
 
 const geist = Geist({
